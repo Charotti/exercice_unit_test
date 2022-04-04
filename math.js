289de4f5667d0494082bc64ae785d3462f0099c7
unknown <clamy6@yahoo.fr>
2022-04-04 15:46:56 +0200

    isEven bonus

--- math.js
+++ math.js
@@ -1,8 +1,11 @@
-function even(number) {
-  if (number % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
+// function even(number) {
+//   if (number % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+function even(n) {
+  return parseInt(n) % 2 === 0;
 }
 module.exports = even;
--- test.js
+++ test.js
@@ -1,11 +1,20 @@
 const even = require("./math");
 
-test("if provided parameter is even return true", () => {
-  const result = even(4);
-  expect(result).toBe(true);
+// test("if provided parameter is even return true", () => {
+//   const result = even(4);
+//   expect(result).toBe(true);
+// });
+
+// test("if provided parameter is odd number return false", () => {
+//   const result = even(3);
+//   expect(result).toBe(false);
+// });
+test("if provided parameter is string return false", () => {
+  const result = even("str");
+  expect(result).toBe(false);
 });
 
-test("if provided parameter is odd number return false", () => {
-  const result = even(3);
-  expect(result).toBe(false);
+test("if provided parameter is a string representing even number return true", () => {
+  const result = even("24");
+  expect(result).toBe(true);
 });
