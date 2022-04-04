9837652f196473721dfdfd8b5db318a762bbd199
unknown <clamy6@yahoo.fr>
2022-04-04 16:08:58 +0200

    formDate

--- math.js
+++ math.js
@@ -5,7 +5,19 @@
 //     return false;
 //   }
 // }
-function even(n) {
-  return parseInt(n) % 2 === 0;
+//Even bonus
+// function even(n) {
+//   return parseInt(n) % 2 === 0;
+// }
+
+//formatDate
+
+function formDate(d) {
+  const date = new Date(d);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${day}/${month}/${year}`;
 }
-module.exports = even;
+
+module.exports = formDate;
--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-const even = require("./math");
+// const even = require("./math");
+const formDate = require("./math");
 
 // test("if provided parameter is even return true", () => {
 //   const result = even(4);
@@ -9,12 +10,16 @@ const even = require("./math");
 //   const result = even(3);
 //   expect(result).toBe(false);
 // });
-test("if provided parameter is string return false", () => {
-  const result = even("str");
-  expect(result).toBe(false);
-});
+// test("if provided parameter is string return false", () => {
+//   const result = even("str");
+//   expect(result).toBe(false);
+// });
 
-test("if provided parameter is a string representing even number return true", () => {
-  const result = even("24");
-  expect(result).toBe(true);
+// test("if provided parameter is a string representing even number return true", () => {
+//   const result = even("24");
+//   expect(result).toBe(true);
+// });
+test("change date format from yyyy-mm-dd to dd/mm/yyyy", () => {
+  const result = formDate("2022-04-01");
+  expect(result).toBe("01/04/2022");
 });
